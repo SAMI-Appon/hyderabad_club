371ba11bd78c83dcdea405c9ece5b17e772d1da8
zia qureshi <ziaqureshi4200@gmail.com>
2020-11-24 17:03:14 +0500

    bookingRoute

--- routes/web.php
+++ routes/web.php
@@ -329,6 +329,8 @@ Route::middleware(['setData', 'auth', 'SetSessionData', 'language', 'timezone', 
 
     Route::get('bookings/get-todays-bookings', 'Restaurant\BookingController@getTodaysBookings');
     Route::resource('bookings', 'Restaurant\BookingController');
+    Route::resource('bookings/rooms', 'Restaurant\BookingController@roomlist');
+
     Route::get('get-rooms', 'Restaurant\BookingController@getRooms')->name('get_rooms');
     Route::get('get-members', 'Restaurant\BookingController@getFamilyMembers')->name('get_members');
 
